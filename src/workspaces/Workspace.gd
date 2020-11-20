4e78442bba79b4eb9f1bd64397241a425f769db7
Phil Zona <phil.b.zona@gmail.com>
2020-11-20 15:30:07 -0600

    Remove debug lines

--- src/workspaces/Workspace.gd
+++ src/workspaces/Workspace.gd
@@ -3,9 +3,7 @@ extends Control
 enum tabs {CREATE = 0, ANALYZE = 1, REFINE = 2, EXPORT = 3}
 
 func set_create_tab() -> void:
-	print(tabs.CREATE)
 	$Container.current_tab = tabs.CREATE
 
 func set_export_tab() -> void:
-	print(tabs.EXPORT)
 	$Container.current_tab = tabs.EXPORT
