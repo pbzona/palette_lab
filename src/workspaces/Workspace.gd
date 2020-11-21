db9f701c626f5ba6fe81b84817925fd664dc252f
Phil Zona <phil.b.zona@gmail.com>
2020-11-21 17:02:37 -0600

    Rename set tab methods to public

--- src/workspaces/Workspace.gd
+++ src/workspaces/Workspace.gd
@@ -7,10 +7,10 @@ var palette = null
 func _ready():
 	$Container/Create.connect("PALETTE_UPDATED", self, "set_workspace_palette")
 
-func _set_create_tab() -> void:
+func set_create_tab() -> void:
 	$Container.current_tab = tabs.CREATE
 
-func _set_export_tab() -> void:
+func set_export_tab() -> void:
 	$Container.current_tab = tabs.EXPORT
 
 func _set_workspace_palette(p) -> void:
